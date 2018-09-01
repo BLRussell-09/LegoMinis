b31a333b87e9c78f064e7fd9f17e12993e92a799
Barry Russell <blrussell09@gmail.com>
2018-09-01 13:22:30 -0500

    Add project files.

---
+++ LegoMinis/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoMinis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
